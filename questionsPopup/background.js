5cb2337b8350ff5c55ddebda9ecb587b29ed9225
KhushiKV <78869960+KhushiKV@users.noreply.github.com>
2021-08-18 20:05:23 +0530

    questions popup

---
+++ questionsPopup/background.js
@@ -0,0 +1,8 @@
+// console.log("Background is also heree!!");
+//
+// chrome.browserAction.onClicked.addListener(buttonClicked);
+//
+// function buttonClicked(tab) {
+//   let msg = { "txt" : "Hello"};
+//   chrome.tabs.sendMessage(tab.id, msg);
+// }
