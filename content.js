996250f05cb0b1426ac43ad542e0181713616a32
Gaurav Jain <gjain369@gmail.com>
2021-08-18 23:21:27 +0530

    resolved

--- content.js
+++ content.js
@@ -1,21 +1,25 @@
 const observer = new MutationObserver((mutationsList, observer) => {
-    //     console.log(mutationsList);
-    console.log("=================");
     const childs = mutationsList[0].addedNodes[0].children;
     const status = childs[1].querySelector("a").innerText;
-    // const runtime = childs[2].innerText;
+    const runtime = childs[2].innerText;
     // const memory = childs[2].innerText;
-    console.log(status/*, runtime, memory */);
-    // alert(`status: ${status}\nruntime: ${runtime}\nmemory: ${memory}`);
+    console.log(status, runtime/*, memory */);
+    alert(`status: ${status}\nruntime: ${runtime}`);
     chrome.runtime.sendMessage({
         status : status,
-        // runtime : runtime,
+        runtime : runtime,
         // memory : memory
     });
-}
-);
+});
 
-observer.observe(document.querySelector(".ant-table-body > table > tbody"),
-    { attributes: true, childList: true, subtree: true });
+t = setInterval(()=>{
+    if(document.querySelector(".submit__2ISl")){
+        document.querySelector(".submit__2ISl").addEventListener('click',()=>{
+            observer.observe(document.querySelector(".ant-table-body > table > tbody"),
+                { attributes: true, childList: true, subtree: true });
+        });
+        clearInterval(t);
+    }
+},1000)
 
 // observer.disconnect();
