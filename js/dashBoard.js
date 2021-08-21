6fe92e9959f24ef2d72db6bbd2ee63c23858c2f6
ash-100 <77534181+ash-100@users.noreply.github.com>
2021-08-21 11:15:54 +0530

    Update dashBoard.js

--- js/dashBoard.js
+++ js/dashBoard.js
@@ -1,5 +1,5 @@
 function signOut(){
-    window.location.replace('popup1.html')
+    window.location.replace('signIn.html')
     chrome.runtime.sendMessage({message:"Sign Out"},function(response){
         // sends a message to sign out 
     })
