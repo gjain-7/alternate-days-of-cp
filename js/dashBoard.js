7e9b2b989818a1244dd0389de4aace04e615c3f4
ash-100 <77534181+ash-100@users.noreply.github.com>
2021-09-15 23:53:36 +0530

    Update dashBoard.js

--- js/dashBoard.js
+++ js/dashBoard.js
@@ -1,6 +1,15 @@
-function signOut(){
-    window.location.replace('signIn.html')
+chrome.storage.sync.get(['user'],function(result){
+    const userDetails= result.user;
+    document.getElementById('userName').innerHTML=userDetails.displayName;
+    document.getElementById('profileImage').src=userDetails.photoURL;
+})
+
+const signOutButton=document.getElementById('signOut')
+signOutButton.addEventListener('click',()=>{
     chrome.runtime.sendMessage({message:"Sign Out"},function(response){
-        // sends a message to sign out 
-    })
-}
+        if(response.message=='success'){
+            window.location.replace('/signIn.html')
+        }       
+    });
+});
+    
