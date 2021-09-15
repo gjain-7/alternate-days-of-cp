aad0b4d1c918d3a32d59f18319869d693b9278f2
ash-100 <77534181+ash-100@users.noreply.github.com>
2021-09-15 23:52:28 +0530

    Update signIn.js

--- js/signIn.js
+++ js/signIn.js
@@ -1,8 +1,6 @@
-
-chrome.runtime.sendMessage({message:'isUserSignedIn'},function(response){        
-    if(response.result){
-        window.location.replace('dashBoard.html');            
-    }
+var port = chrome.runtime.connect({name: "port1"});
+port.postMessage({message: "isUserSignedIn"});
+port.onMessage.addListener(function(msg) {
+  if (msg.signedInStatus)
+    window.location.replace('/dashBoard.html');            
 });
-
-
